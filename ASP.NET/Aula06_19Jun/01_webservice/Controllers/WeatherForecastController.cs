a516ad63495a8bf4b95259f92db132a713485709
Edson Moreno <edson.moreno@pucrs.br>
2024-06-19 21:04:05 -0300

    Fechamento do curso, apresentando webapi

---
+++ ASP.NET/Aula06_19Jun/01_webservice/Controllers/WeatherForecastController.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc;
+using _01_webservice.Models;
+
+namespace _01_webservice.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+
+    [HttpPost]
+    public string Acao01([FromBody] string nome){
+        return $"Ola {nome}";
+    }
+
+    [HttpPost("CEP")]
+    public IActionResult Acao02(CEPModel cep){
+
+        Console.WriteLine($"{cep.Nome}; {cep.ID}");
+        return Ok(cep);
+
+    }
+
+
+}
---
+++ ASP.NET/Aula06_19Jun/01_webservice/Models/CEPModel.cs
@@ -0,0 +1,7 @@
+namespace _01_webservice.Models;
+public class CEPModel
+{
+    public string Nome { get; set; }
+    public int ID { get; set; }
+    
+}
